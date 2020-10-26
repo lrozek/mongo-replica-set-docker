22d58b5334bdc1ecb12c9d5ec07f2bc03a4aee8f
lrozek <lukasz.rozek84@gmail.com>
2020-10-26 18:10:33 +0100

    replicate set specific config

--- rs-init/init_replica_set.js
+++ rs-init/init_replica_set.js
@@ -7,11 +7,14 @@ config = {
         },
         {
             _id: 1,
-            host: "mongo-replica-set-docker_mongo_2"
+            host: "mongo-replica-set-docker_mongo_2",
+            priority: 0,
+            slaveDelay: 10
         },
         {
             _id: 2,
-            host: "mongo-replica-set-docker_mongo_3"
+            host: "mongo-replica-set-docker_mongo_3",
+            priority: 2
         }
       ]
 }
