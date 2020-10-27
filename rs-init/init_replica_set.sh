d431a91a7ed6cef8a782cb1dcd72f260f9fb1032
lrozek <lukasz.rozek84@gmail.com>
2020-10-27 11:59:07 +0100

    replica set initialization executed inside container instead of host

--- rs-init/init_replica_set.sh
+++ rs-init/init_replica_set.sh
@@ -1,27 +1,27 @@
 function getContainerIpAddress(){
 	docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$1"	
 }
-
-MONGO_HOST_1=$(getContainerIpAddress "mongo-replica-set-docker_mongo_1")
-MONGO_HOST_2=$(getContainerIpAddress "mongo-replica-set-docker_mongo_2")
-MONGO_HOST_3=$(getContainerIpAddress "mongo-replica-set-docker_mongo_3")
-cat <<EOF > rs-init/init_replica_set.js
+MONGO_HOST_1=mongo-replica-set-docker_mongo_1
+MONGO_IP_1=$(getContainerIpAddress $MONGO_HOST_1)
+MONGO_IP_2=$(getContainerIpAddress "mongo-replica-set-docker_mongo_2")
+MONGO_IP_3=$(getContainerIpAddress "mongo-replica-set-docker_mongo_3")
+mongo --host $MONGO_HOST_1 <<EOF
 config = {
     _id: "rs0",
     members: [
         {
             _id: 0,
-            host: "${MONGO_HOST_1}"
+            host: "${MONGO_IP_1}"
         },
         {
             _id: 1,
-            host: "${MONGO_HOST_2}",
+            host: "${MONGO_IP_2}",
             priority: 0,
             slaveDelay: 10
         },
         {
             _id: 2,
-            host: "${MONGO_HOST_3}",
+            host: "${MONGO_IP_3}",
             priority: 2
         }
       ]
@@ -29,4 +29,3 @@ config = {
 var result = rs.initiate(config)
 printjson(result)
 EOF
-docker exec -w /rs-init mongo-replica-set-docker_mongo_1 mongo init_replica_set.js
